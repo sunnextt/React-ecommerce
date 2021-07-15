f5184fcbae478710cf1036ea8a97bddd33c0056e
sunnextt <sunnext2010@gmail.com>
2021-07-15 03:55:04 +0100

    implementing react app and redux with firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+// firebase config
+export const firebaseConfig = {
+    apiKey: "AIzaSyBdnHfQGAPswfAV1xCBDCKwEg6ccYp8sqA",
+    authDomain: "ecommerce-website-71213.firebaseapp.com",
+    projectId: "ecommerce-website-71213",
+    storageBucket: "ecommerce-website-71213.appspot.com",
+    messagingSenderId: "157492005563",
+    appId: "1:157492005563:web:fcdfb957dedf21607a379e"
+};
